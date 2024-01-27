0d7515e74b63609058d21aac27eca0ef91ca7124
Jorge Emiliano Maldonado <69151487+jorgeemilianom@users.noreply.github.com>
2024-01-27 16:03:34 -0300

    fix migrations. Add backoffice configutariont

--- Commands/Migrations.php
+++ Commands/Migrations.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Coreshell\Commands;
 
 use Coreshell\Library\AbstractConsoleLibrary;
@@ -7,25 +8,55 @@ class Migrations extends AbstractConsoleLibrary
 {
 
     public static function migrate($DB)
-    { 
-        $rutaDirectorio = 'Project/src/Modules'; // Ruta del directorio principal 'Modules'
+    {
+        # Load migrations if backend client exist
+        $loadFolderMigrations = 'Project/backend/Endpoints.php';
+        if (file_exists($loadFolderMigrations)) {
+            echo " [●] Buscando migraciones del backend del cliente \n";
+            $rutaDirectorio = 'Project/backend/Modules'; // Ruta del directorio principal 'Modules'
+            $arrayQueries = [];
+            self::buscarArchivoMigrations($rutaDirectorio, $arrayQueries); // Llamar a la función para buscar el archivo 'Migrations.sql' dentro del directorio
+            if (!$arrayQueries) {
+                echo " [x] No se encontraron migraciones \n";
+            } else {
+                echo " [●] Ejecutando migraciones \n";
+                foreach ($arrayQueries as $keyPath => $queries) {
+                    echo "      [►] Ejecutando migraciones de $keyPath \n";
+                    foreach ($queries as $query) {
+                        if (!$query || empty($query)) continue;
+
+                        $status = (bool)$DB::query(trim($query));
+                        if ($status) {
+                            echo "          [√] " . substr(trim(str_replace(array("\r", "\n"), '', $query)), 0, 70) . " ... \n";
+                        } else {
+                            echo "          [x] " . substr(trim(str_replace(array("\r", "\n"), '', $query)), 0, 70) . " ... \n";
+                        }
+                    }
+                }
+            }
+        }
+
+        # Load backoffice migrations
+        echo " [●] Ejecutando migraciones de BackOffice \n";
+        $rutaDirectorio = 'Backoffice/backend/Modules'; // Ruta del directorio principal 'Modules'
         $arrayQueries = [];
         self::buscarArchivoMigrations($rutaDirectorio, $arrayQueries); // Llamar a la función para buscar el archivo 'Migrations.sql' dentro del directorio
-        if(!$arrayQueries) echo " [x] No se encontraron migraciones \n";
+        if (!$arrayQueries) {
+            echo " [x] No se encontraron migraciones \n";
+        } else {
+            echo " [●] Ejecutando migraciones \n";
+            foreach ($arrayQueries as $keyPath => $queries) {
+                echo "      [►] Ejecutando migraciones de $keyPath \n";
+                foreach ($queries as $query) {
+                    if (!$query || empty($query)) continue;
 
-        echo " [●] Ejecutando migraciones \n";
-        foreach ($arrayQueries as $keyPath => $queries) {
-            echo "      [►] Ejecutando migraciones de $keyPath \n";
-            foreach ($queries as $query) {
-                if(!$query || empty($query)) continue;
-                
-                $status = (bool)$DB::query(trim($query));
-                if($status){
-                    echo "          [√] ".substr(trim(str_replace(array("\r", "\n"), '', $query)), 0, 70)." ... \n";
-                }else{
-                    echo "          [x] ".substr(trim(str_replace(array("\r", "\n"), '', $query)), 0, 70)." ... \n";
+                    $status = (bool)$DB::query(trim($query));
+                    if ($status) {
+                        echo "          [√] " . substr(trim(str_replace(array("\r", "\n"), '', $query)), 0, 70) . " ... \n";
+                    } else {
+                        echo "          [x] " . substr(trim(str_replace(array("\r", "\n"), '', $query)), 0, 70) . " ... \n";
+                    }
                 }
-                
             }
         }
     }
